45f8d9be62ce6ad4b661e8c42a030620c40ad8c6
FahadZaheerfzr <fahadzaheer19@gmail.com>
2023-06-01 01:31:10 +0500

    Fixed stupid bugs

--- make_password.js
+++ make_password.js
@@ -3,7 +3,7 @@ const bcrypt = require('bcrypt');
 
 
 const generatePassword = async () => {
-    const password = "randomPassword"
+    const password = "password"
     const hash = await bcrypt.hash(password, 0);
     console.log(hash);
 }
